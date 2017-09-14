65253fb48f47bbb60517b9a4a2c6b044490ccada
cooperbuilt <evanncooper@gmail.com>
2017-09-14 08:51:22 -0400

    Change route in email reset to proper route, fix markup in reset view

--- controllers/authController.js
+++ controllers/authController.js
@@ -42,7 +42,7 @@ exports.forgot = async (req, res) => {
     user.resetPasswordExpires = Date.now() + 3600000; // One Hour from Now
     await user.save();
     // Send email with the token
-    const resetURL = `http://${req.headers.host}.account/reset/${user.resetPasswordToken}`;
+    const resetURL = `http://${req.headers.host}/account/reset/${user.resetPasswordToken}`;
     await mail.send({
         user,
         subject: 'Password Reset',
