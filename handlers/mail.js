0e770515477aa99a5a1371539b6e8bf6003c66d9
cooperbuilt <evanncooper@gmail.com>
2017-09-14 08:38:14 -0400

    Adjust email sender to admin to comply with postmark

--- handlers/mail.js
+++ handlers/mail.js
@@ -24,7 +24,7 @@ exports.send = async (options) => {
   const html = generateHTML(options.filename, options);
   const text = htmlToText.fromString(html);
   const mailOptions = {
-    from: `The Trubadour Team <noreply@trubadour.com`,
+    from: `The Trubadour Team <admin@trubadour.com`,
     to: options.user.email,
     subject: options.subject,
     html,
